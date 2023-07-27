9e0cc87ed3c269ad5a5c622f071f11bf93c348f9
surbhigoyal2002 <134638406+surbhigoyal2002@users.noreply.github.com>
2023-07-27 19:58:21 +0530

    Add files via upload

---
+++ firebase/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDd_96dn1iaZ6Tzsqsq9bxQo_LJSPSJTX4",
+    authDomain: "find-teammate-c37b8.firebaseapp.com",
+    projectId: "find-teammate-c37b8",
+    storageBucket: "find-teammate-c37b8.appspot.com",
+    messagingSenderId: "163574034190",
+    appId: "1:163574034190:web:bfa8656ad5ca5fc836f15f",
+    measurementId: "G-V3KDV4JTH1"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.auth();
